d23292f85ad572db09aefb7c1a0f8165ddbe87fb
TuanTran <tamthy12345@gmail.com>
2024-02-13 20:05:56 +0700

    add new module + clean repo

---
+++ server/my_modules/school/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'School Management',
+    'version': '1.0',
+    'author': 'Tran Dang Tuan',
+    'summary': 'Used for school management',
+    'description': 'Used for school management',
+    'depends': [
+        # 'base',
+        # 'other_module'
+    ],
+    'data': [
+        # 'views/my_view.xml',
+        # 'data/demo_data.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
